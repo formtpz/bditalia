865e6bf46d3642b168c6ab2f13767f48c539ff2d
formtpz <137564298+formtpz@users.noreply.github.com>
2026-01-06 11:28:16 -0600

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,4 @@ import streamlit as st
 
 @st.cache_resource
 def get_connection():
-    return psycopg2.connect(
-        host="HOST",
-        database="DB",
-        user="USER",
-        password="PASSWORD"
-    )
+    return psycopg2.connect(os.environ["DB_URI"])
