e66231d1acc6224af0ee8bc51238e1d30e3662dc
formtpz <137564298+formtpz@users.noreply.github.com>
2026-01-06 10:53:48 -0600

    creacion app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from auth import login_usuario
+
+st.set_page_config(page_title="Sistema de Reportes", layout="centered")
+
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = False
+
+if not st.session_state.authenticated:
+    st.image("logo.png", use_container_width=True)
+    st.title("Ingreso al sistema")
+
+    cedula = st.text_input("Cédula")
+    password = st.text_input("Contraseña", type="password")
+
+    if st.button("Ingresar"):
+        login_usuario(cedula, password)
+else:
+    st.success(f"Bienvenido {st.session_state.nombre}")
+    st.info("Use el menú lateral para navegar")
