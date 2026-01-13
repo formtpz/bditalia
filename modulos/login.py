2516ad3184cc3cd725027ea0ba986f85cb686082
formtpz <137564298+formtpz@users.noreply.github.com>
2026-01-13 13:17:16 -0600

    Update and rename Login.py to modulos/login.py

---
+++ modulos/login.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from auth import login_usuario
+
+def render():
+    st.image("logo.png", use_container_width=True)
+
+    st.title("Ingreso al sistema")
+
+    cedula = st.text_input("Cédula")
+    password = st.text_input("Contraseña", type="password")
+
+    if st.button("Ingresar"):
+        login_usuario(cedula, password)
