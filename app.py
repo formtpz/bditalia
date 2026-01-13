4e8d25f717d214edd4ab7eb92035383366c03540
formtpz <137564298+formtpz@users.noreply.github.com>
2026-01-13 15:54:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ perfil = usuario["perfil"]
 opciones = PERMISOS_POR_PERFIL.get(perfil, [])
 
 with st.sidebar:
-    st.image("logo.png", use_container_width=True)
-    st.image(logo, width=12) 
+    st.image("logo.png", use_container_width=True, width=120)
+
     st.markdown("### Menú")
     opcion = st.radio("Seleccione una opción", opciones)
 
