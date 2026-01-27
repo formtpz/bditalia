0d4ceadb4dfdc3814cec6aa0c2d656fd5c639c16
formtpz <137564298+formtpz@users.noreply.github.com>
2026-01-27 13:13:51 -0600

    Update correcciones.py

--- modulos/correcciones.py
+++ modulos/correcciones.py
@@ -42,7 +42,7 @@ def render():
             FROM reportes
             WHERE fecha_reporte BETWEEN %s AND %s AND cedula_personal = %s
             ORDER BY fecha_reporte DESC
-        """, conn, params=[fecha_inicio, fecha_fin, cedula]
+        """, conn, params=[fecha_inicio, fecha_fin, cedula])
 
         st.info("Seleccione visualmente el registro con error y copie el ID")
         st.dataframe(df_registros, use_container_width=True)
