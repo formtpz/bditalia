85fe8ea175799ada6ad041e6ae8f9b194e01011d
formtpz <137564298+formtpz@users.noreply.github.com>
2026-02-17 15:56:02 -0600

    Update produccion.py

--- modulos/produccion.py
+++ modulos/produccion.py
@@ -17,7 +17,7 @@ def render():
     puesto = usuario["puesto"]
     cedula_usuario = usuario["cedula"]
 
-    if perfil not in (1, 3, 4):
+    if perfil not in (1, 3, 4, 5):
         st.error("No tiene permiso para acceder a Producción")
         st.stop()
 
